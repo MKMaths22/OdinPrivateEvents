54c03c8af14bb93523acc45f8cc3bd9be3d24e22
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-01-07 15:05:23 +0000

    Generate registrations controller and add routes, remembering to restart the server to see the sign_up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: "users/sessions" }
+  devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  resources :users, only: [:create, :update, :show]
   resources :events, only: [:index, :show]
   root 'events#index'
   get "up" => "rails/health#show", as: :rails_health_check
