55fc7a8a00a96c939e32ffdfa78fb2381bdc116e
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-01-10 11:15:34 +0000

    Add #new and #create to Events controller, adding the routes to routes.rb Add "Create Event" button to _sign_up_in_or_out partial and make new Event view form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,4 +5,26 @@ class EventsController < ApplicationController
 
   def show
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(allowed_event_params)
+    @event[:creator_id] = current_user[:id]
+
+    if @event.save
+      redirect_to root_path
+    else
+      flash.now[:alert] = @event.errors.full_messages
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private 
+
+  def allowed_event_params
+    params.require(:event).permit(:location, :date)
+  end
 end
