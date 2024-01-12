b936fe1d7eca2dccda249cec3b591af0d1deb90b
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-01-12 11:53:13 +0000

    Remove debugging commands in Bookings controller. Add link_single_events variable to _display_events_partial to show individual event links in Events index view but not show view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,9 @@
 class BookingsController < ApplicationController
   def create
     @booking = Booking.new
-    puts params
     @booking.attended_event_id = params[:event_id]
     @booking.attendee_id = current_user.id
-    p @booking
-    if @booking.save
-      puts 'booking has been saved'
-    else
-      puts 'saving failed'
-    end
+    @booking.save
     redirect_to root_path
   end
 
