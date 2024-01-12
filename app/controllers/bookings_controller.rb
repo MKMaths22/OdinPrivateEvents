11d1091d77a70dd8d41eaf1ef7a2f2eaead89012
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-01-12 11:32:38 +0000

    Write #create in Bookings controller with link_to in _display_events partial to create a booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,16 @@
 class BookingsController < ApplicationController
   def create
-    @booking = Booking.new(attended_event_id: = params[:id], attendee_id: = current_user.id)
+    @booking = Booking.new
+    puts params
+    @booking.attended_event_id = params[:event_id]
+    @booking.attendee_id = current_user.id
+    p @booking
+    if @booking.save
+      puts 'booking has been saved'
+    else
+      puts 'saving failed'
+    end
+    redirect_to root_path
   end
 
   def destroy
