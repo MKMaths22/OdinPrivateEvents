cb2587ab05c2dc37681932179a12c259a1072337
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-01-13 14:19:56 +0000

    Add uniqueness of the two Bookings columns to model, rewriting #create in Bookings controller to provide flash message if uniqueness fails

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,8 +3,10 @@ class BookingsController < ApplicationController
     @booking = Booking.new
     @booking.attended_event_id = params[:event_id]
     @booking.attendee_id = current_user.id
-    @booking.save
-    redirect_to root_path
+    unless @booking.save
+      flash[:alert] = "Booking failed to save"
+    end
+    redirect_to event_path(id: @booking.attended_event_id)
   end
 
   def destroy
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :attended_event, :class_name => "Event"
   belongs_to :attendee, :class_name => "User"
+  validates :attendee, uniqueness: { scope: :attended_event }
 end
