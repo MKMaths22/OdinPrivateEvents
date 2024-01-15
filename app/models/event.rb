23c712862065098ee712ca371f9f887e39cd7378
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-01-15 10:03:05 +0000

    Add default scope to Event model to always order by date

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,5 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
   has_many :bookings, foreign_key: 'attended_event_id', dependent: :destroy
   has_many :attendees, :through => :bookings
+  default_scope {order('date')}
 end
