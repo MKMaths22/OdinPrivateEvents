a2785bf7db20ffe5af31098e0b2ff125dd8951ff
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-01-16 10:59:28 +0000

    Add depdendent:destroy everywhere there is has_many in User and Event models. Deleting an Event causes Foreign Key Exception if there are any attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
   validates :date, presence: true
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
   has_many :bookings, foreign_key: 'attended_event_id', dependent: :destroy
-  has_many :attendees, :through => :bookings
+  has_many :attendees, :through => :bookings, dependent: :destroy
   default_scope {order('date')}
 
   scope :past, -> { where(date: ..(Time.now))}
