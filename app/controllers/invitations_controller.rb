65209cdf0602d775b2c11efedc4c7c96a1594a94
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-01-18 10:27:51 +0000

    Flesh out #create and #update in Invitations controller

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,7 +1,23 @@
 class InvitationsController < ApplicationController
   def create
+    @invitation = Invitation.new(allowed_invite_params)
+    if @invitation.save
+      redirect_to event_path(id: invited_event_id)
+    else
+      flash.now[:alert] = @invitation.errors.full_messages
+      render event_path(id: invited_event_id), status: :unprocessable_entity
+    end
   end
 
   def update
+    @invitation = Invitation.find(params[:id])
+    @invitation = @invitation.update(:declined => true)
+    redirect_to event_path(id: invited_event_id)
+  end
+
+  private
+
+  def allowed_invite_params
+    params.require(:invitation).permit(:invited_event_id, :invitee_id)
   end
 end
