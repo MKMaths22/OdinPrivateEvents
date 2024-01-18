3c1a9348bd40dc536c43f8df6121b5e6d0200c88
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-01-18 12:14:20 +0000

    Add associations to Invitation model. Modify _display_events partial for accepting/declining invitations. Start to change Events show view for issuing/observing invitations

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,2 +1,5 @@
 class Invitation < ApplicationRecord
+  belongs_to :invited_event, :class_name => "Event"
+  belongs_to :invitee, :class_name => "User"
+  validates :invitee, uniqueness: { scope: :invited_event }
 end
