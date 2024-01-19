cedef7d32484736b0b6007a84ce04554a7f137e7
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-01-19 17:49:48 +0000

    Not sure why Line 19 in Events #show view causes an error. The console allows me to obtain a User from an Invitation using the invitee method, but when I put such a method call in the Events #show view, I get NoMethodError

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,4 +2,5 @@ class Invitation < ApplicationRecord
   belongs_to :invited_event, :class_name => "Event"
   belongs_to :invitee, :class_name => "User"
   validates :invitee, uniqueness: { scope: :invited_event }
+  scope :declined, -> { where(declined: true) }
 end
