485380b1d7adb55a6f2a80fb373682927a30f2f6
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-01-20 10:40:46 +0000

    Modify #allowed_invite_params to allow link to #create Invitation in Event #show view. List pending invitations in that view and tidy up with <br> linebreaks.

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,22 +2,22 @@ class InvitationsController < ApplicationController
   def create
     @invitation = Invitation.new(allowed_invite_params)
     if @invitation.save
-      redirect_to event_path(id: invited_event_id)
+      redirect_to event_path(id: params[:invited_event_id])
     else
       flash.now[:alert] = @invitation.errors.full_messages
-      render event_path(id: invited_event_id), status: :unprocessable_entity
+      render event_path(id: params[:invited_event_id]), status: :unprocessable_entity
     end
   end
 
   def update
     @invitation = Invitation.find(params[:id])
     @invitation = @invitation.update(:declined => true)
-    redirect_to event_path(id: invited_event_id)
+    redirect_to event_path(id: params[:invited_event_id])
   end
 
   private
 
   def allowed_invite_params
-    params.require(:invitation).permit(:invited_event_id, :invitee_id)
+    params.permit(:invited_event_id, :invitee_id)
   end
 end
