668fb95ceaf558bc5cbdf0d4545c8292e37f2810
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-01-21 15:03:25 +0000

    Fix bugs due to Event.find and User.find lines in Invitations controller not using params

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -4,7 +4,7 @@ class InvitationsController < ApplicationController
   before_action :authenticate_invitee, only: [:update]
 
   def authenticate_creator
-    event = Event.find(:invited_event_id)
+    event = Event.find(params[:invited_event_id])
     unless current_user && current_user == event.creator
       flash[:alert] = 'You must be logged in as the Event creator to make an Invitation.'
       redirect_to new_user_session_path
@@ -12,8 +12,8 @@ class InvitationsController < ApplicationController
   end
 
   def authenticate_invitee
-    event = Event.find(:invited_event_id)
-    invitee = User.find(:invitee_id)
+    event = Event.find(params[:invited_event_id])
+    invitee = User.find(params[:invitee_id])
     unless current_user && event.invitees.include?(current_user)
       flash[:alert] = 'You must be logged in and invited to this Event to decline an Invitation.'
       redirect_to new_user_session_path
