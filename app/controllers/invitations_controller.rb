3b8fa2bca4b193d8008e50ebee550a7e400df63d
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-01-21 11:54:01 +0000

    Add before_actions #authenticate_creator and #authenticate_invitee to Invitations Controller to ensure that only the creator can make an Invitation and only the invitee can decline it.

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,4 +1,25 @@
 class InvitationsController < ApplicationController
+  
+  before_action :authenticate_creator, only: [:create]
+  before_action :authenticate_invitee, only: [:update]
+
+  def authenticate_creator
+    event = Event.find(:invited_event_id)
+    unless current_user && current_user == event.creator
+      flash[:alert] = 'You must be logged in as the Event creator to make an Invitation.'
+      redirect_to new_user_session_path
+    end
+  end
+
+  def authenticate_invitee
+    event = Event.find(:invited_event_id)
+    invitee = User.find(:invitee_id)
+    unless current_user && event.invitees.include?(current_user)
+      flash[:alert] = 'You must be logged in and invited to this Event to decline an Invitation.'
+      redirect_to new_user_session_path
+    end
+  end
+  
   def create
     @invitation = Invitation.new(allowed_invite_params)
     if @invitation.save
